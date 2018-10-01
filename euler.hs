4eeadb83f6c1b806f7ed4665f36b201a10e61a0e
Caio Alonso <33733676+csalonso@users.noreply.github.com>
2018-10-01 13:34:10 -0300

    Update euler.hs

--- euler.hs
+++ euler.hs
@@ -1,4 +1,8 @@
 module Euler where
 
-soma :: Int -> Int
-soma x = sum [i | i <- [0..x-1], i `mod` 3 == 0 || i `mod` 5 == 0]
+--1
+euler1 :: Int -> Int
+euler1 x = sum [i | i <- [0..x-1], i `mod` 3 == 0 || i `mod` 5 == 0]
+
+--2
+euler2 :: Int -> Int
