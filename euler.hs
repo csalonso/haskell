5a12517d286f8e245e185b7b04c98bd0492acd74
Caio Alonso <33733676+csalonso@users.noreply.github.com>
2018-10-02 17:21:59 -0300

    Update euler.hs

--- euler.hs
+++ euler.hs
@@ -11,4 +11,14 @@ fib 1 = 1
 fib x = fib (x - 1) + fib (x - 2)
 
 euler2 :: Int -> Int
-euler2 x = sum [fib i | i <- [0..x], fib i `mod` 2 /= 0]
+euler2 x = sum [fib i | i <- [0..x], fib i `mod` 2 /= 0, fib i < 4000000]
+
+--3
+isPrime :: Int -> Bool
+isPrime x = length [x `mod` i | i <- [1..x], x `mod` i == 0] == 2
+
+listPrimes :: Int -> [Int]
+listPrimes x = [i | i <- [1..x], isPrime i == True]
+
+euler3 :: Int -> Int
+euler3 x = maximum [i | i <- [1..x], isPrime i == True, x `mod` i == 0]
